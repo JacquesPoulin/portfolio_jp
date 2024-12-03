cb133dccd55a73ca9437e4f57d40d759eb2894c3
jacquesPoulin <jacques.poulin64@gmail.com>
2024-12-03 19:48:41 +0100

    MAJ du CV

--- src/app/curriculum/Curriculum.tsx
+++ src/app/curriculum/Curriculum.tsx
@@ -183,7 +183,7 @@ export const Curriculum = () => {
 							<div className='flex flex-col md:flex-row md:justify-between md:items-center mb-4'>
 								<div>
 									<h3 className='font-semibold text-xl'>
-										Développeur Full Stack - SCANIA
+										Développeur Full Stack - CODEACTIVE
 									</h3>
 									<p className='text-white/60'>2022 - Présent</p>
 								</div>
